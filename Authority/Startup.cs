8c0de03c6ce7a27631693f6364194cb9c712ed59
Alex <Alex@DESKTOP-DF13GSC>
2022-09-11 00:15:14 +0300

    Add project files.

---
+++ Authority/Startup.cs
@@ -0,0 +1,67 @@
+using Authority.Utility;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authority
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddIdentityServer()
+                    .AddInMemoryClients(Config.Clients)
+                    .AddInMemoryIdentityResources(Config.IdentityResources)
+                    .AddInMemoryApiResources(Config.ApiResources)
+                    .AddInMemoryApiScopes(Config.ApiScopes)
+                    .AddTestUsers(Config.TestUsers)
+                    .AddDeveloperSigningCredential();
+
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseIdentityServer();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
---
+++ Authority/Utility/Config.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System.Collections.Generic;
+
+namespace Authority.Utility
+{
+    public static class Config
+    {
+        public static IEnumerable<Client> Clients =>
+        new Client[]
+        {
+            new Client
+             {
+                 ClientId = "userFlowClient",
+                 AllowedGrantTypes = GrantTypes.ClientCredentials,
+                 ClientSecrets =
+                     {
+                     new Secret("secret".Sha256())
+                     },
+                 AllowedScopes = { "userFlow" }
+             }
+        };
+        public static IEnumerable<ApiScope> ApiScopes =>
+         new ApiScope[]
+         {
+             new ApiScope("userFlow", "user Flow")
+         };
+        public static IEnumerable<ApiResource> ApiResources =>
+         new ApiResource[]
+         {
+         };
+        public static IEnumerable<IdentityResource> IdentityResources =>
+         new IdentityResource[]
+         {
+         };
+        public static List<TestUser> TestUsers =>
+         new List<TestUser>
+         {
+         };
+    }
+}
