2a915f1e4b02660443efbef926ceacf16572baf0
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2023-01-01 20:08:47 +0200

    some fixes

---
+++ esport.ui/pages/api/user/confirm/[token].ts
@@ -0,0 +1,15 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { authService } from "@api/auth/authService";
+
+type Data = {
+  name: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  authService.confirm((req.query.token as string) ?? "");
+  res.status(200).json({ name: "John Doe" });
+}
