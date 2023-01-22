3c7cd2ccd36acc21f1e4a68cbf6ed00e1ab4a5e3
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2023-01-22 23:13:53 +0200

    0.0.14 - added missing export

--- esport.lib-ts/src/competitions/contracts/queries/index.ts
+++ esport.lib-ts/src/competitions/contracts/queries/index.ts
@@ -2,5 +2,8 @@
 export * from "./competition.get-all-competitions.query";
 export * from "./competition.get-competition-by-id.query";
 
+//category
+export * from "./category.get-category-by-id.query";
+
 //fight
 export * from "./fight.get-all.query";
