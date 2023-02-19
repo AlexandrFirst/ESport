36a0f91e713ef4f3035cfc013ee826b64d6dcdca
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2023-02-19 21:55:31 +0200

    deleted reject

--- esport.ui/pages/competition/index.tsx
+++ esport.ui/pages/competition/index.tsx
@@ -4,15 +4,15 @@ import { NextPage } from "next";
 import { MainLayout } from "@layouts/MainLayout";
 
 import { ICompetition } from "@entities/competition";
+import { getAppServerSideProps } from "@app/store";
 
 import { CompetitionsGrid } from "@page-widgets/page-all-competitions";
-import { getAppServerSideProps } from "@app/store/lib/getAppServerSideProps";
 
 type PageProps = {
   competitions: ICompetition[];
 };
 
-const CompetitionPage: NextPage<PageProps> = ({ competitions }) => {
+const CompetitionPage: NextPage<PageProps> = ({ competitions = [] }) => {
   return (
     <MainLayout>
       <CompetitionsGrid competitions={competitions} />
@@ -22,24 +22,12 @@ const CompetitionPage: NextPage<PageProps> = ({ competitions }) => {
 
 export default CompetitionPage;
 
-export const getServerSideProps = getAppServerSideProps<PageProps>(
-  async () => {
-    const data = await new Promise<{ competitions: ICompetition[] }>(
-      (resolve, reject) => reject({ competitions: [] })
-    );
+export const getServerSideProps = getAppServerSideProps<PageProps>(async () => {
+  const data = await new Promise<{ competitions: ICompetition[] }>((resolve) =>
+    resolve({ competitions: [] })
+  );
 
-    return {
-      props: data,
-    };
-  },
-  ["competition"],
-  {
-    onReject: () => {
-      return {
-        props: {
-          competitions: [],
-        },
-      };
-    },
-  }
-);
+  return {
+    props: data,
+  };
+});
