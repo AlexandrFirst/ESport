d71c78d907bc61d502842988db2b5d2b22af69e1
Alexandr Logvinov <ato31ato@gmail.com>
2023-03-01 00:39:28 +0200

    all keys added

--- IdentityV2/Startup.cs
+++ IdentityV2/Startup.cs
@@ -11,7 +11,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Routing;
