0a780b759dc510bf302f7a5f87c0be875c4b803d
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2023-05-02 16:47:19 +0300

    configured SSL certs for ui2 and CORS in Identity

--- esport.ui2/src/_app/server.ts
+++ esport.ui2/src/_app/server.ts
@@ -1,28 +1,43 @@
 import next from "next";
-import { createServer } from "https";
-import { parse } from "url";
+import {createServer as HTTPSCreateServer} from "https";
+import {createServer as HTTPCreateServer} from "http";
+import {parse} from "url";
 import fs from "fs";
 import path from "path";
+// import { loadEnvConfig } from "@next/env";
 
 const port = parseInt(process.env.PORT || "3000", 10);
 const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev, hostname: "localhost", port });
+const app = next({ dev, port });
 const handle = app.getRequestHandler();
 
-const httpsOptions = {
-  key: fs.readFileSync(path.resolve(".cerfs", "localhost-key.pem")),
-  cert: fs.readFileSync(path.resolve(".cerfs", "localhost.pem")),
-};
-
 app.prepare().then(() => {
-  createServer(httpsOptions, (req, res) => {
-    const parsedUrl = parse(req.url!, true);
-    handle(req, res, parsedUrl);
-  }).listen(port);
+  console.log("===process.env===", process.env);
+
+  const httpsOptions = {
+    key: fs.readFileSync(
+      path.resolve(".cerfs", process.env.SSL_KEY_NAME ?? "")
+    ),
+    cert: fs.readFileSync(
+      path.resolve(".cerfs", process.env.SSL_CERT_NAME ?? "")
+    ),
+  };
+
+  if (!dev) {
+    HTTPSCreateServer(httpsOptions, (req, res) => {
+      const parsedUrl = parse(req.url!, true);
+      handle(req, res, parsedUrl);
+    }).listen(port);
+  } else {
+    HTTPCreateServer((req, res) => {
+      const parsedUrl = parse(req.url!, true);
+      handle(req, res, parsedUrl);
+    }).listen(port);
+  }
 
   console.log(
-    `> Server listening at https://localhost:${port} as ${
-      dev ? "development" : process.env.NODE_ENV
-    }`
+    dev
+      ? `> Server listening at http://localhost:${port}`
+      : `> Server listening at https://localhost:${port}`
   );
 });
