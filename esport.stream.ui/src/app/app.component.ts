0a8c9ad4d4762b74fd832333f79abf89fdc2f8e4
Alexandr Logvinov <ato31ato@gmail.com>
2023-06-03 11:22:55 +0300

    in progress

--- esport.stream.ui/src/app/app.component.ts
+++ esport.stream.ui/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
+import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -7,16 +8,26 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  
-  constructor(public cookieService: CookieService) {
-      console.log(cookieService.getAll());
+
+  constructor(public cookieService: CookieService, private theme: ThemeService) {
+    console.log(cookieService.getAll());
   }
-  
+
   title = 'esport.stream.ui';
-  
+
   ngOnInit(): void {
-    
+    const local = this;
+    window.addEventListener(
+      "message",
+      function (event) {
+        let data = event.data;
+        if (data != undefined && (data == 'light' || data == 'dark')) {
+          console.log(data)
+          local.theme.current = data;
+        }
+
+      },
+      false
+    );
   }
-
-
 }
