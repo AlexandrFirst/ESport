a04c7f6a52b69058dc2bab79e9d6f51e14fc7373
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2023-06-08 02:18:42 +0300

    fixed styles for checkbox

--- esport.ui2/src/shared/ui/Checkbox/CheckboxBase.tsx
+++ esport.ui2/src/shared/ui/Checkbox/CheckboxBase.tsx
@@ -5,39 +5,51 @@ import { CheckboxProps } from "@radix-ui/react-checkbox";
 import cn from "classnames";
 
 import { Check } from "lucide-react";
+import { ErrorMessage } from "@/shared/types";
+import { FormError } from "../FormError/FormError";
 
 export type CheckboxBaseProps = CheckboxProps & {
   label?: string;
+  error?: ErrorMessage;
 };
 
 export const CheckboxBase = forwardRef<
   ElementRef<typeof CheckboxPrimitive.Root>,
   CheckboxBaseProps
->(({ className, label, name, ...props }, ref) => (
-  <div className={cn(styles.wrapper, className)}>
-    <CheckboxPrimitive.Root
-      ref={ref}
-      id={name}
-      name={name}
-      className={cn(
-        styles.checkbox,
-        styles.disabled,
-        styles.focus,
-        styles.checked
-      )}
-      {...props}
-    >
-      <CheckboxPrimitive.Indicator
-        className={cn("flex items-center justify-center text-current")}
-      >
-        <Check className="h-4 w-4" />
-      </CheckboxPrimitive.Indicator>
-    </CheckboxPrimitive.Root>
-    {label && (
-      <label htmlFor={name} className={styles.label}>
-        {label}
-      </label>
-    )}
-  </div>
-));
+>(({ className, label, name, error, ...props }, ref) => {
+  return (
+    <div className={className}>
+      <div className={styles.wrapper}>
+        <CheckboxPrimitive.Root
+          ref={ref}
+          id={name}
+          name={name}
+          className={cn(
+            styles.checkbox,
+            styles.disabled,
+            styles.focus,
+            styles.checked,
+            { [styles.error]: !!error }
+          )}
+          {...props}
+        >
+          <CheckboxPrimitive.Indicator
+            className={cn("flex items-center justify-center text-current")}
+          >
+            <Check className="h-4 w-4" />
+          </CheckboxPrimitive.Indicator>
+        </CheckboxPrimitive.Root>
+        {label && (
+          <label
+            htmlFor={name}
+            className={cn(styles.label, { [styles.error]: !!error })}
+          >
+            {label}
+          </label>
+        )}
+      </div>
+      <FormError error={error} />
+    </div>
+  );
+});
 CheckboxBase.displayName = CheckboxPrimitive.Root.displayName;
