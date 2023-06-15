66da000e08dafc176a12b28193d731ec85360b02
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2023-06-15 20:49:33 +0300

    added captcha

--- esport.ui2/src/_app/Providers/ui/Providers.tsx
+++ esport.ui2/src/_app/Providers/ui/Providers.tsx
@@ -11,6 +11,7 @@ import { LOCAL_STORAGE_THEME_KEY } from "../lib/ThemeContext";
 
 import { QueryProvider } from "./QueryProvider";
 import { StoreProvider } from "./StoreProvider";
+import { ReCaptchaProvider } from "next-recaptcha-v3";
 
 interface ProvidersProps {
   store: Store<CombinedState<StateSchema>>;
@@ -31,7 +32,11 @@ export const Providers: FC<ProvidersProps> = ({
         // themes={["pink", "red", "blue", "light", "dark"]}
       >
         <Snackbar>
-          <QueryProvider pageProps={pageProps}>{children}</QueryProvider>
+          <QueryProvider pageProps={pageProps}>
+            <ReCaptchaProvider reCaptchaKey="6LdUdJ4mAAAAAEH11QOssCwO6M9zS7sIkreb2qCZ">
+              {children}
+            </ReCaptchaProvider>
+          </QueryProvider>
         </Snackbar>
       </ThemeProvider>
     </StoreProvider>
